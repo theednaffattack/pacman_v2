4aa953cc9984dec7667e336b80a57466fcc85840
Eddie Naff <eddie@eddienaff.dev>
2023-02-04 12:35:59 -0800

    Create a pellet class

---
+++ src/pellet-class.ts
@@ -0,0 +1,23 @@
+import { PositionType } from "./types";
+import { context, canvasErrorString } from "./main";
+
+export class Pellet {
+  position: PositionType;
+  radius: number;
+  constructor({ position }: { position: PositionType }) {
+    this.position = position;
+    this.radius = 3;
+  }
+
+  draw() {
+    if (!context) {
+      console.error(canvasErrorString);
+      return;
+    }
+    context.beginPath();
+    context.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    context.fillStyle = "white";
+    context.fill();
+    context.closePath();
+  }
+}
