b84f32bde3e282cb94592a142e0c3701889302e1
Eddie Naff <eddienaff@gmail.com>
2023-03-06 17:31:14 -0800

    Add visual debugging helper

---
+++ src/draw-path-line.ts
@@ -0,0 +1,32 @@
+import { TILE_SIZE } from "./constants";
+import { Ghost } from "./ghost-class";
+
+export function drawPathLine({
+  ghost,
+  context,
+}: {
+  context: CanvasRenderingContext2D;
+  ghost: Ghost;
+}) {
+  // Draw a line of the ghost's path
+  for (
+    let pathIndex = 0;
+    pathIndex < ghost.ghostPenPath.length - 1;
+    pathIndex++
+  ) {
+    const [x0, y0] = ghost.ghostPenPath[pathIndex];
+    const [x1, y1] = ghost.ghostPenPath[pathIndex + 1];
+
+    context.beginPath();
+    context.moveTo(
+      x0 * TILE_SIZE + TILE_SIZE / 2,
+      y0 * TILE_SIZE + TILE_SIZE / 2
+    );
+    context.lineTo(
+      x1 * TILE_SIZE + TILE_SIZE / 2,
+      y1 * TILE_SIZE + TILE_SIZE / 2
+    );
+    context.strokeStyle = "#32CD32";
+    context.stroke();
+  }
+}
